4f8e4ca51e76bd4216f18c290738eb6e33f252c9
KeyboardKillerG <keyboardkiller.gk@gmail.com>
2020-09-03 19:38:53 -0500

    RecyclerView Adapter Java classs generated

--- app/src/main/java/com/dodhall/quicky/HistoryItemAdapter.java
+++ app/src/main/java/com/dodhall/quicky/HistoryItemAdapter.java
@@ -1,4 +1,32 @@
 package com.dodhall.quicky;
 
-public class HistoryItemAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistoryItemAdapter extends RecyclerView.Adapter<HistoryItemAdapter.ItemViewHolder> {
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
